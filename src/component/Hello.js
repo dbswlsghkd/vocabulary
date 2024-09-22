6057cff3680304f51c04f14eef77ad1ceabc2328
황윤진 <dbswlsghkd@gmail.com>
2024-09-22 17:13:49 +0900

    리액트 컴포넌트 만들기

---
+++ src/component/Hello.js
@@ -0,0 +1,12 @@
+import World from './World';
+
+const Hello = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <World></World>
+        </div>
+    );
+};
+
+export default Hello;
