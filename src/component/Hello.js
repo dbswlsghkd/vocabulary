841097e0dc5ab28403cbffe2253a32f727a46ad2
황윤진 <dbswlsghkd@gmail.com>
2024-09-23 13:54:58 +0900

    state, useState 이해

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,27 +1,48 @@
 import World from './World';
 
-const showName = () => {
-    console.log('Mike');
-};
+// 이벤트 처리
+// const showName = () => {
+//     console.log('Mike');
+// };
 
-const showAge = (age) => {
-    console.log(age);
-};
+// const showAge = (age) => {
+//     console.log(age);
+// };
 
-const Hello = () => {
+// const Hello = () => {
+//     return (
+//         <div>
+//             <h1>Hello</h1>
+//             <button onClick={showName}>Show name</button>
+//             <button
+//                 onClick={() => {
+//                     showAge(10);
+//                 }}
+//             >
+//                 Show age
+//             </button>
+//         </div>
+//     );
+// };
+
+import { useState } from 'react';
+
+export default function Hello() {
+    // let name = 'Mike';
+    const [name, setName] = useState('Mike');
+
+    const changeName = () => {
+        const newName = name === 'Mike' ? 'Jane' : 'Mike';
+        setName(newName);
+        // console.log(name);
+        // document.getElementById('name').innerText = name;
+    };
+
     return (
         <div>
-            <h1>Hello</h1>
-            <button onClick={showName}>Show name</button>
-            <button
-                onClick={() => {
-                    showAge(10);
-                }}
-            >
-                Show age
-            </button>
+            <h1>state</h1>
+            <h2 id="name">{name}</h2>
+            <button onClick={changeName}>Change</button>
         </div>
     );
-};
-
-export default Hello;
+}
