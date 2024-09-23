03dd23b7a48b47c40c1f91a9b562d3f0d9ba4313
황윤진 <dbswlsghkd@gmail.com>
2024-09-23 14:08:10 +0900

    props의 이해

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -26,14 +26,20 @@ import World from './World';
 // };
 
 import { useState } from 'react';
+import UserName from './UserName';
 
-export default function Hello() {
+export default function Hello(props) {
     // let name = 'Mike';
     const [name, setName] = useState('Mike');
+    // props를 직접적으로 변경하는것은 불가능
+    const [age, setAge] = useState(props.age);
+
+    const msg = age > 19 ? '성인 입니다.' : '미성년자 입니다.';
 
     const changeName = () => {
         const newName = name === 'Mike' ? 'Jane' : 'Mike';
         setName(newName);
+        setAge(age + 1);
         // console.log(name);
         // document.getElementById('name').innerText = name;
     };
@@ -41,7 +47,10 @@ export default function Hello() {
     return (
         <div>
             <h1>state</h1>
-            <h2 id="name">{name}</h2>
+            <h2 id="name">
+                {name}({age}) : {msg}
+            </h2>
+            <UserName name={name}></UserName>
             <button onClick={changeName}>Change</button>
         </div>
     );
