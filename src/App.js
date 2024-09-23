f9bf1d803b9b78265ae11a5394100ea2188e718b
황윤진 <dbswlsghkd@gmail.com>
2024-09-23 18:10:00 +0900

    react-router-dom 이해

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import './App.css';
 import DayList from './component/DayList';
 import Header from './component/Header';
 import Day from './component/Day';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import EmptyPage from './component/EmptyPage';
 
 function App() {
     return (
-        <div className="App">
-            <Header></Header>
-            <DayList></DayList>
-            <Day></Day>
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <Header></Header>
+                <Routes>
+                    <Route path="/" exact element={<DayList></DayList>}></Route>
+                    <Route path="/day/:day" exact element={<Day></Day>}></Route>
+                    <Route path="*" element={<EmptyPage></EmptyPage>}></Route>
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
--- src/component/Day.js
+++ src/component/Day.js
@@ -1,11 +1,14 @@
 import dummy from '../db/data.json';
+import { useParams } from 'react-router-dom';
 
 export default function Day() {
-    const day = 1;
-    const wordList = dummy.words.filter((word) => word.day === day);
-    console.log(wordList);
+    const useP = useParams();
+    const day = useP.day;
+    const wordList = dummy.words.filter((word) => word.day === Number(day));
+
     return (
         <>
+            <h2>Day {day}</h2>
             <table>
                 <tbody>
                     {wordList.map((word) => (
