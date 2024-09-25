4e2962313e4f858b27d32bac7ca329d801af5ae0
황윤진 <dbswlsghkd@gmail.com>
2024-09-25 15:44:12 +0900

    useNavigate 구현하여 단어 추가 시 해당 페이지로 이동 되도록 수정

--- src/App.js
+++ src/App.js
@@ -2,18 +2,28 @@ import './App.css';
 import DayList from './component/DayList';
 import Header from './component/Header';
 import Day from './component/Day';
+import CreateWord from './component/CreateWord';
+import CreateDay from './component/CreateDay';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import EmptyPage from './component/EmptyPage';
 
 function App() {
     return (
         <BrowserRouter>
-            <div className="App">
+            <div className="index">
                 <Header></Header>
                 <Routes>
                     <Route path="/" exact element={<DayList></DayList>}></Route>
                     <Route path="/day/:day" exact element={<Day></Day>}></Route>
                     <Route path="*" element={<EmptyPage></EmptyPage>}></Route>
+                    <Route
+                        path="/create_word"
+                        element={<CreateWord></CreateWord>}
+                    ></Route>
+                    <Route
+                        path="/create_day"
+                        element={<CreateDay></CreateDay>}
+                    ></Route>
                 </Routes>
             </div>
         </BrowserRouter>
