24074b18389330582c1b3dab561fca21c55d3534
황윤진 <dbswlsghkd@gmail.com>
2024-09-26 17:14:03 +0900

    Day 앞뒤로 이동가능한 버튼 구현

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,17 +1,47 @@
 import { useParams } from 'react-router-dom';
 import Word from './Word';
 import useFetch from '../hooks/useFetch';
+import { Link } from 'react-router-dom';
 
 export default function Day() {
     const { day } = useParams();
-
+    const days = useFetch('http://localhost:3001/days');
     const words = useFetch(`http://localhost:3001/words?day=${day}`);
 
     return (
         <>
             <h2>Day {day}</h2>
             {words.length === 0 && <span>Loading...</span>}
-            <table>
+
+            <table
+                style={{
+                    width: '80%',
+                    marginLeft: 'auto',
+                    marginRight: 'auto',
+                }}
+            >
+                <div style={{ float: 'left' }}>
+                    <Link
+                        className="button"
+                        to={`/day/${
+                            Number(day) === 1 ? days.length : Number(day) - 1
+                        }`}
+                    >
+                        Prev
+                    </Link>
+                </div>
+                <div style={{ float: 'right' }}>
+                    <Link
+                        className="button"
+                        to={`/day/${
+                            Number(day) === days.length
+                                ? Number(1)
+                                : Number(day) + 1
+                        }`}
+                    >
+                        Next
+                    </Link>
+                </div>
                 <tbody>
                     {words.map((word) => (
                         <Word word={word} key={word.id}></Word>
